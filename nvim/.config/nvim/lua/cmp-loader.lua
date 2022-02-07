a752eea3471e106ac00f68ddeb55e6287df9df6b
Adrien Charruel <adrien.charruel@tagheuer.com>
2022-02-07 13:44:05 +0100

    nvim: Disable completition inside comments

--- nvim/.config/nvim/lua/cmp-loader.lua
+++ nvim/.config/nvim/lua/cmp-loader.lua
@@ -94,4 +94,12 @@ cmp.setup {
             return vim_item
         end,
     },
+    enabled = function()
+        local in_prompt = vim.api.nvim_buf_get_option(0, 'buftype') == 'prompt'
+        if in_prompt then  -- this will disable cmp in the Telescope window (taken from the default config)
+            return false
+        end
+        local context = require("cmp.config.context")
+        return not(context.in_treesitter_capture("comment") == true or context.in_syntax_group("Comment"))
+    end
 }
