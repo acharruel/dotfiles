99f5faf7d81e44d82b1ab36e304789f47c6da334
Adrien Charruel <adrien.charruel@tagheuer.com>
2022-03-11 15:16:54 +0100

    nvim: Improve togglecolumn

--- nvim/.config/nvim/lua/togglecolumn.lua
+++ nvim/.config/nvim/lua/togglecolumn.lua
@@ -4,16 +4,12 @@ local M = {}
 M._ctx = {}
 
 -- Toggle colorcolumn option
---   if set colorcolumn is stored to internal context and set to 0
 --   if unset colorcolumn is restored from context (if available)
+--   if set colorcolumn is stored to internal context and set to 0
 M.toggle_colorcolumn = function()
     cc = vim.o.colorcolumn
     if cc == "0" then
-        new_cc = "0"
-        if M._ctx.prev_cc then
-            new_cc = M._ctx.prev_cc
-        end
-        vim.o.colorcolumn = new_cc
+        vim.o.colorcolumn = M._ctx.prev_cc or "0"
     else
         M._ctx.prev_cc = cc
         vim.o.colorcolumn = "0"
