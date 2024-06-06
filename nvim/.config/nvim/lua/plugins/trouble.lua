fc3801682c0fabd88beb67163710ba103778e8b2
Adrien Charruel <adrien.charruel@tagheuer.com>
2024-06-06 18:26:08 +0200

    nvim: Update trouble configuration

--- nvim/.config/nvim/lua/plugins/trouble.lua
+++ nvim/.config/nvim/lua/plugins/trouble.lua
@@ -1,23 +1,22 @@
 local M = {
     "folke/trouble.nvim",
-    branch = "dev", -- IMPORTANT!
     cmd = "Trouble",
     opts = {}, -- for default options, refer to the configuration section for custom setup.
 }
 
 local key_opts = {
-    mode = "n", -- NORMAL mode
+    mode = "n",     -- NORMAL mode
     prefix = "<leader>",
-    buffer = nil, -- Global mappings. Specify a buffer number for buffer local mappings
-    silent = true, -- use `silent` when creating keymaps
+    buffer = nil,   -- Global mappings. Specify a buffer number for buffer local mappings
+    silent = true,  -- use `silent` when creating keymaps
     noremap = true, -- use `noremap` when creating keymaps
-    nowait = true, -- use `nowait` when creating keymaps
+    nowait = true,  -- use `nowait` when creating keymaps
 }
 
 local mappings = {
     t = {
-        ["d"] = { "<cmd>Trouble diagnostics toggle<cr>", "[T]rouble [D]iagnostics" },
-        ["s"] = { "<cmd>Trouble lsp_documents_symbols toggle<cr>", "[T]rouble [S]ymbols" },
+        ["d"] = { "<cmd>Trouble diagnostics toggle focus=false<cr>", "[T]rouble [D]iagnostics" },
+        ["s"] = { "<cmd>Trouble symbols toggle pinned=true results.win.relative=win resuls.win.position=right<cr>", "[T]rouble [S]ymbols" },
     },
 }
 
