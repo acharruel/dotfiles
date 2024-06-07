9dfdb3f0bab8b017f069c3332159a9596e0967eb
Adrien Charruel <adrien.charruel@tagheuer.com>
2024-06-07 10:20:01 +0200

    nvim: Move venv plugin to regexp branch

--- nvim/.config/nvim/lua/plugins/venv-selector.lua
+++ nvim/.config/nvim/lua/plugins/venv-selector.lua
@@ -1,6 +1,7 @@
 return {
     "linux-cultist/venv-selector.nvim",
     dependencies = { "neovim/nvim-lspconfig", "nvim-telescope/telescope.nvim" },
+    branch = "regexp",
     config = true,
     event = "VeryLazy", -- Optional: needed only if you want to type `:VenvSelect` without a keymapping
 }
