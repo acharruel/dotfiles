55d742b74dd482d5356935b0284e2e2545732432
Adrien Charruel <adrien.charruel@gmail.com>
2024-10-06 11:10:08 +0200

    nvim: Add markview.nvim plugin

---
+++ nvim/.config/nvim/lua/plugins/markview.lua
@@ -0,0 +1,12 @@
+local M = {
+    "OXY2DEV/markview.nvim",
+    lazy = false,      -- Recommended
+    -- ft = "markdown" -- If you decide to lazy-load anyway
+
+    dependencies = {
+        "nvim-treesitter/nvim-treesitter",
+        "nvim-tree/nvim-web-devicons"
+    }
+}
+
+return M
