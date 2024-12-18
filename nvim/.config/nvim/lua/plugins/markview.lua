013595e365bb9c1fc6d832e8aa49b19b9dabb5d0
Adrien Charruel <acharruel@nanoxplore.com>
2024-12-18 11:00:59 +0100

    nvim: Lazyload markview plugin

--- nvim/.config/nvim/lua/plugins/markview.lua
+++ nvim/.config/nvim/lua/plugins/markview.lua
@@ -1,7 +1,7 @@
 local M = {
     "OXY2DEV/markview.nvim",
-    lazy = false,      -- Recommended
-    -- ft = "markdown" -- If you decide to lazy-load anyway
+    lazy = true,      -- Recommended
+    ft = "markdown", -- If you decide to lazy-load anyway
 
     dependencies = {
         "nvim-treesitter/nvim-treesitter",
