70112c712f725f28479110d8c558c5d9229bb928
Adrien Charruel <adrien.charruel@gmail.com>
2024-12-25 17:33:38 +0100

    nvim: Replace nvim-cmp by blink

--- nvim/.config/nvim/lua/lsp/handlers.lua
+++ nvim/.config/nvim/lua/lsp/handlers.lua
@@ -60,13 +60,6 @@ M.on_attach = function(_, bufnr)
     lsp_keymaps(bufnr)
 end
 
-local capabilities = vim.lsp.protocol.make_client_capabilities()
-
-local status_ok, cmp_nvim_lsp = pcall(require, "cmp_nvim_lsp")
-if not status_ok then
-    return
-end
-
-M.capabilities = cmp_nvim_lsp.default_capabilities(capabilities)
+M.capabilities = require('blink.cmp').get_lsp_capabilities()
 
 return M
---
+++ nvim/.config/nvim/lua/plugins/blink.lua
@@ -0,0 +1,68 @@
+local M = {
+  'saghen/blink.cmp',
+  -- optional: provides snippets for the snippet source
+  dependencies = {
+    'rafamadriz/friendly-snippets',
+  },
+  build = "cargo build --release",
+
+  opts = {
+    -- 'default' for mappings similar to built-in completion
+    -- 'super-tab' for mappings similar to vscode (tab to accept, arrow keys to navigate)
+    -- 'enter' for mappings similar to 'super-tab' but with 'enter' to accept
+    -- see the "default configuration" section below for full documentation on how to define
+    -- your own keymap.
+    keymap = { preset = 'enter' },
+
+    appearance = {
+      -- Sets the fallback highlight groups to nvim-cmp's highlight groups
+      -- Useful for when your theme doesn't support blink.cmp
+      -- will be removed in a future release
+      use_nvim_cmp_as_default = true,
+      -- Set to 'mono' for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
+      -- Adjusts spacing to ensure icons are aligned
+      nerd_font_variant = 'mono'
+    },
+
+    -- default list of enabled providers defined so that you can extend it
+    -- elsewhere in your config, without redefining it, due to `opts_extend`
+    sources = {
+      default = { 'lsp', 'path', 'snippets', 'buffer' },
+      -- optionally disable cmdline completions
+      cmdline = {},
+    },
+
+    -- experimental signature help support
+    signature = {
+      enabled = true,
+      window = { border = "rounded" },
+    },
+
+    completion = {
+      menu = {
+        draw = {
+          columns = {
+            { "kind_icon", "label", "label_description", gap = 1 },
+            { "kind", gap = 1 },
+          },
+        },
+        border = "rounded",
+      },
+
+      -- Show documentation when selecting a completion item
+      documentation = {
+        auto_show = true, auto_show_delay_ms = 500,
+        window = { border = "rounded" },
+      },
+
+      -- Display a preview of the selected item on the current line
+      ghost_text = { enabled = false },
+    },
+
+  },
+  -- allows extending the providers array elsewhere in your config
+  -- without having to redefine it
+  opts_extend = { "sources.default" }
+}
+
+return M
