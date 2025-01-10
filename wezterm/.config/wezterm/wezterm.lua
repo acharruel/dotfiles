aa04dbcce94ab269b1272e78ae7d0eeb05b93daa
Adrien Charruel <acharruel@nanoxplore.com>
2025-01-10 14:01:46 +0100

    wezterm: Add some more key bindings with LEADER

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -37,4 +37,94 @@ config.hide_mouse_cursor_when_typing = true
 -- window apha channel opacity
 config.window_background_opacity = 0.95
 
+-- Keymaps
+-- config.disable_default_key_bindings = true
+-- timeout_milliseconds defaults to 1000 and can be omitted
+config.leader = { key = 'w', mods = 'ALT', timeout_milliseconds = 1000 }
+config.keys = {
+    -- general functions
+    {
+        key = 'p',
+        mods = 'LEADER',
+        action = wezterm.action.ActivateCommandPalette,
+    },
+    {
+        key = 'n',
+        mods = 'LEADER',
+        action = wezterm.action.SpawnWindow,
+    },
+    {
+        key = 'U',
+        mods = 'LEADER',
+        action = wezterm.action.CharSelect,
+    },
+    {
+        key = 'U',
+        mods = 'LEADER',
+        action = wezterm.action.CharSelect,
+    },
+
+    -- panes management
+    {
+        key = '|',
+        mods = 'LEADER',
+        action = wezterm.action.SplitHorizontal { domain = 'CurrentPaneDomain' },
+    },
+    {
+        key = '-',
+        mods = 'LEADER',
+        action = wezterm.action.SplitVertical { domain = 'CurrentPaneDomain' },
+    },
+    {
+        key = 'LeftArrow',
+        mods = 'LEADER',
+        action = wezterm.action.ActivatePaneDirection 'Left',
+    },
+    {
+        key = 'RightArrow',
+        mods = 'LEADER',
+        action = wezterm.action.ActivatePaneDirection 'Right',
+    },
+    {
+        key = 'UpArrow',
+        mods = 'LEADER',
+        action = wezterm.action.ActivatePaneDirection 'Up',
+    },
+    {
+        key = 'DownArrow',
+        mods = 'LEADER',
+        action = wezterm.action.ActivatePaneDirection 'Down',
+    },
+    {
+        key = 'f',
+        mods = 'LEADER',
+        action = wezterm.action.ToggleFullScreen,
+    },
+    {
+        key = 'PageUp',
+        mods = 'SHIFT',
+        action = wezterm.action.ScrollByPage(-1),
+    },
+    {
+        key = 'PageDown',
+        mods = 'SHIFT',
+        action = wezterm.action.ScrollByPage(1),
+    },
+
+    -- tabs management
+    {
+        key = 't',
+        mods = 'LEADER',
+        action = wezterm.action.SpawnTab 'DefaultDomain',
+    },
+}
+
+wezterm.on('update-right-status', function(window, pane)
+    local leader = ''
+    if window:leader_is_active() then
+        leader = 'LEADER'
+    end
+    window:set_right_status(leader)
+end)
+
 return config
