f9a410258edc555c074175be3d89ecf1d146c566
Adrien Charruel <adrien.charruel@gmail.com>
2025-02-27 11:26:53 +0100

    wezterm: use catppuccin theme

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -7,7 +7,7 @@ local config = wezterm.config_builder()
 -- This is where you actually apply your config choices
 
 -- colorscheme
-config.color_scheme = 'Nord (Gogh)'
+config.color_scheme = 'Catppuccin Frappe'
 
 -- font
 config.font = wezterm.font('Hack Nerd Font',
