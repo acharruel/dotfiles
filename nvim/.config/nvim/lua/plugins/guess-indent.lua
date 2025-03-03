6c24abad4fbd4dda8546d8d8315e400148000b7d
Adrien Charruel <acharruel@nanoxplore.com>
2025-03-03 10:29:48 +0100

    nvim: Update guess-indent config

--- nvim/.config/nvim/lua/plugins/guess-indent.lua
+++ nvim/.config/nvim/lua/plugins/guess-indent.lua
@@ -16,6 +16,15 @@ function M.config()
             "terminal",
             "prompt",
         },
+        on_tab_options = { -- A table of vim options when tabs are detected
+            ["expandtab"] = false,
+        },
+        on_space_options = { -- A table of vim options when spaces are detected
+            ["expandtab"] = true,
+            ["tabstop"] = "detected", -- If the option value is 'detected', The value is set to the automatically detected indent size.
+            ["softtabstop"] = "detected",
+            ["shiftwidth"] = "detected",
+        },
     }
 end
 
