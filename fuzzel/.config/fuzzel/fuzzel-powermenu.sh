c8b86ec969da838724e614ea519386f8b91219de
Adrien Charruel <adrien.charruel@gmail.com>
2025-05-07 16:32:53 +0200

    Replace sherlock by fuzzel

---
+++ fuzzel/.config/fuzzel/fuzzel-powermenu.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+SELECTION="$(printf "\
+Lock\\0icon\\x1f/home/adrien/.local/share/icons/Tela-circle/24/actions/system-lock-screen.svg\n\
+Suspend\\0icon\\x1f/home/adrien/.local/share/icons/Tela-circle/24/actions/system-suspend.svg\n\
+Log out\\0icon\\x1f/home/adrien/.local/share/icons/Tela-circle/24/actions/system-log-out.svg\n\
+Reboot\\0icon\\x1f/home/adrien/.local/share/icons/Tela-circle/24/actions/system-reboot.svg\n\
+Shutdown\\0icon\\x1f/home/adrien/.local/share/icons/Tela-circle/24/actions/system-shutdown.svg" | fuzzel --dmenu -p "Power Menu: ")"
+
+case $SELECTION in
+    *"Lock")
+        swaylock;;
+    *"Suspend")
+        systemctl suspend;;
+    *"Log out")
+        swaymsg exit;;
+    *"Reboot")
+        systemctl reboot;;
+    *"Reboot to UEFI")
+        systemctl reboot --firmware-setup;;
+    *"Shutdown")
+        systemctl poweroff;;
+esac
