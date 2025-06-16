d2f267563bd55be7552884febc37bafdb6081286
Adrien Charruel <acharruel@nanoxplore.com>
2025-06-16 11:14:30 +0200

    nvim: New simple LSP icons

--- nvim/.config/nvim/lua/plugins/lsp.lua
+++ nvim/.config/nvim/lua/plugins/lsp.lua
@@ -26,10 +26,10 @@ local M = {
 local function configure_diagnostics()
     local signs = {
         text = {
-            [vim.diagnostic.severity.ERROR] = '',
-            [vim.diagnostic.severity.WARN] = '',
-            [vim.diagnostic.severity.HINT] = '',
-            [vim.diagnostic.severity.INFO] = '',
+            [vim.diagnostic.severity.ERROR] = '󰝥',
+            [vim.diagnostic.severity.WARN] = '󰝥',
+            [vim.diagnostic.severity.HINT] = '󰝥',
+            [vim.diagnostic.severity.INFO] = '󰝥',
         },
         numhl = {
             [vim.diagnostic.severity.ERROR] = 'ErrorMsg',
