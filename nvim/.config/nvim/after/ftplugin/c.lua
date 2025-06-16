49f1788c75b91819c1a116f56c1592ce8b544c05
Adrien Charruel <acharruel@nanoxplore.com>
2025-06-16 11:13:20 +0200

    nvim: Update C and python ftplugins

--- nvim/.config/nvim/after/ftplugin/c.lua
+++ nvim/.config/nvim/after/ftplugin/c.lua
@@ -1,6 +1,6 @@
 local set = vim.opt_local
-set.shiftwidth = 8
-set.tabstop = 8
-set.expandtab = false
+set.shiftwidth = 4
+set.tabstop = 4
+set.expandtab = true
 set.list = false
 set.colorcolumn = "80"
