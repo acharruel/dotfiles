341b485e929e8efd8e452f3a3a890f1ca4dc7332
Adrien Charruel <acharruel@nanoxplore.com>
2025-10-30 11:49:12 +0100

    nvim: Better control for inline diag
    - Disable on startup.
    - Add keymap to toggle it.

--- nvim/.config/nvim/lua/plugins/tiny-inline-diagnostic.lua
+++ nvim/.config/nvim/lua/plugins/tiny-inline-diagnostic.lua
@@ -2,9 +2,13 @@ local M = {
     "rachartier/tiny-inline-diagnostic.nvim",
     event = "VeryLazy",
     priority = 1000,
+    keys = {
+        { "<leader>d", "<cmd>TinyInlineDiag toggle<cr>", desc = "Toggle Inline [D]iagnostics" },
+    },
     config = function()
         require("tiny-inline-diagnostic").setup({
             preset = "modern",
+            tranparent_bg = true,
             options = {
                 multilines = {
                     enabled = true,
@@ -13,6 +17,11 @@ local M = {
         })
         vim.diagnostic.config({ virtual_text = false }) -- Disable Neovim's default virtual text diagnostics
         vim.diagnostic.open_float = require("tiny-inline-diagnostic.override").open_float
+    end,
+    init = function()
+        -- disable inline diagnostic on startup
+        local diag = require("tiny-inline-diagnostic")
+        diag.disable()
     end,
 }
 
