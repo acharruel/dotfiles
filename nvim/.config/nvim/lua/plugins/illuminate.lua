4bdcfdc54807dfe3a85a385f718d071d630fd3bc
Adrien Charruel <acharruel@nanoxplore.com>
2025-11-18 11:08:29 +0100

    nvim: Simplify illuminate.nvim configuration

--- nvim/.config/nvim/lua/plugins/illuminate.lua
+++ nvim/.config/nvim/lua/plugins/illuminate.lua
@@ -30,27 +30,8 @@ function M.config()
         },
         large_file_cutoff = 2000,
         large_file_overrides = nil,
+        disable_keymaps = true,
     })
-
-    -- change the highlight style
-    vim.api.nvim_set_hl(0, "IlluminatedWord", { underline = true })
-    vim.api.nvim_set_hl(0, "IlluminatedCurWord", { underline = true })
-    vim.api.nvim_set_hl(0, "IlluminatedWordText", { underline = true })
-    vim.api.nvim_set_hl(0, "IlluminatedWordRead", { underline = true })
-    vim.api.nvim_set_hl(0, "IlluminatedWordWrite", { underline = true })
-
-    -- auto update the highlight style on colorscheme change
-    vim.api.nvim_create_autocmd({ "ColorScheme" },
-        {
-            pattern = { "*" },
-            callback = function(_)
-                vim.api.nvim_set_hl(0, "IlluminatedWord", { underline = true })
-                vim.api.nvim_set_hl(0, "IlluminatedCurWord", { underline = true })
-                vim.api.nvim_set_hl(0, "IlluminatedWordText", { underline = true })
-                vim.api.nvim_set_hl(0, "IlluminatedWordRead", { underline = true })
-                vim.api.nvim_set_hl(0, "IlluminatedWordWrite", { underline = true })
-            end
-        })
 end
 
 return M
