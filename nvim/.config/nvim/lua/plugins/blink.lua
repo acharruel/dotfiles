0a819e7ec7dc1cc8ebe43d7d16c3c9c497a368ed
Adrien Charruel <acharruel@nanoxplore.com>
2026-03-17 13:52:55 +0100

    nvim: Add colorful-menu for better blink completion

--- nvim/.config/nvim/lua/plugins/blink.lua
+++ nvim/.config/nvim/lua/plugins/blink.lua
@@ -47,8 +47,17 @@ M = {
             menu = {
                 draw = {
                     columns = {
-                        { "kind_icon", "label", "label_description", gap = 1 },
-                        { "kind", gap = 1 },
+                        { "kind_icon", "label", gap = 1 },
+                    },
+                    components = {
+                        label = {
+                            text = function(ctx)
+                                return require("colorful-menu").blink_components_text(ctx)
+                            end,
+                            highlight = function(ctx)
+                                return require("colorful-menu").blink_components_highlight(ctx)
+                            end,
+                        },
                     },
                 },
                 border = "rounded",
