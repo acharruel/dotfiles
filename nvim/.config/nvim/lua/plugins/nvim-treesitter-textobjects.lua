4e23649d60b95e79b7cfcd39ad12a9f6c2aff3ed
Adrien Charruel <acharruel@nanoxplore.com>
2026-09-16 11:37:43 +0200

    nvim: Add function parameters textobjects

--- nvim/.config/nvim/lua/plugins/nvim-treesitter-textobjects.lua
+++ nvim/.config/nvim/lua/plugins/nvim-treesitter-textobjects.lua
@@ -22,11 +22,23 @@ local M = {
       },
     })
 
+    -- Selection Keymaps
     local sel = require("nvim-treesitter-textobjects.select")
     vim.keymap.set({ "x", "o" }, "af", function() sel.select_textobject("@function.outer", "textobjects") end)
     vim.keymap.set({ "x", "o" }, "if", function() sel.select_textobject("@function.inner", "textobjects") end)
     vim.keymap.set({ "x", "o" }, "ac", function() sel.select_textobject("@class.outer", "textobjects") end)
     vim.keymap.set({ "x", "o" }, "ic", function() sel.select_textobject("@class.inner", "textobjects") end)
+
+    -- Parameter Selection (using 'a' for argument)
+    vim.keymap.set({ "x", "o" }, "aa", function() sel.select_textobject("@parameter.outer", "textobjects") end)
+    vim.keymap.set({ "x", "o" }, "ia", function() sel.select_textobject("@parameter.inner", "textobjects") end)
+
+    -- Swapping Keymaps
+    local swap = require("nvim-treesitter-textobjects.swap")
+    vim.keymap.set("n", "sp", function() swap.swap_next("@parameter.inner") end,
+      { desc = "Swap parameter with next" })
+    vim.keymap.set("n", "Sp", function() swap.swap_previous("@parameter.inner") end,
+      { desc = "Swap parameter with previous" })
   end,
 }
 
